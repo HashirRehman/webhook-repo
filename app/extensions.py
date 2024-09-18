a9af518eea5465b0107155140488cac0a124a759
HashirRehman <writehashir@gmail.com>
2024-09-18 21:24:13 +0500

    Added MongoDB connection

--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,8 @@
 from flask_pymongo import PyMongo
 
-# Setup MongoDB here
-# mongo = PyMongo(uri="mongodb://localhost:27017/database")
+def init_mongo(app):
+    mongo_uri = "mongodb+srv://hashir:postgres@webhook.qwbok.mongodb.net/webhooks?retryWrites=true&w=majority"
+    app.config['MONGO_URI'] = mongo_uri
+    mongo = PyMongo(app)
+    app.db = mongo.db.github
+    return mongo
